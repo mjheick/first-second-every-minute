9269b75e40b5f70404a3864a5bb243500ed40995
Matthew Heick <mjheick@users.noreply.github.com>
2024-02-23 19:21:17 -0500

    Create 1s3m.php

---
+++ 1s3m.php
@@ -0,0 +1,67 @@
+<?php
+/* Location of dependencies */
+$ffprobe = '/bin/ffprobe';
+$ffmpeg = '/bin/ffmpeg';
+
+/* Defining things */
+if (!file_exists($ffprobe)) { die("ffprobe not found\n"); }
+if (!file_exists($ffmpeg)) { die("ffmpeg not found\n"); }
+if (!isset($argv[1])) { die("need video file as parameter\n"); }
+if (!file_exists($argv[1])) { die("file ${argv[1]} doesn't exist\n"); }
+$input = $argv[1];
+$filename = pathinfo($input, PATHINFO_FILENAME);
+$extension = pathinfo($input, PATHINFO_EXTENSION);
+$output = $filename . '.1sem.' . $extension;
+
+/* Lets get video length */
+$command_output = '';
+$status = exec($ffprobe . ' -v quiet -print_format json -show_format -show_streams ' . escapeshellarg($input), $command_output);
+if ($status === false) { die("error running ffprobe\n"); }
+$json_data = implode('', $command_output);
+$json = json_decode($json_data, true);
+if (is_null($json)) { die("error in decoding output from ffprobe\n"); }
+if (!isset($json['format']['duration'])) { die("cannot retrieve video duration\n"); }
+$duration = $json['format']['duration'];
+
+/* Now we loop every 60 seconds until we're past the video */
+echo "Extracting: ";
+$increment = 0;
+$seconds_progress = 0;
+$minute = 0;
+$hour = 0;
+while ($seconds_progress < $duration) {
+	echo '.';
+	$shell_minute = $minute;
+	if ($minute < 10) { $shell_minute = '0' . $minute; }
+	$incremental_filename = $filename . '-' . $increment . '.' . $extension;
+	if (file_exists($incremental_filename)) {
+		unlink($incremental_filename);
+	}
+	if (!file_exists($incremental_filename)) {
+		exec( $ffmpeg . ' -v quiet -ss ' . $hour . ':' . $shell_minute . ':0.0 -i ' . escapeshellarg($input) . ' -t 1 ' . escapeshellarg($incremental_filename), $throwaway );
+	}
+	$increment = $increment + 1;
+	$minute = $minute + 1;
+	$seconds_progress = $seconds_progress + 60;
+	if ($minute == 60) {
+		$minute = 0;
+		$hour = $hour + 1;
+	}
+}
+echo "\n";
+
+echo "Combining: ";
+/* Combine them all and run the demuxer */
+$fp = fopen($filename . '.txt', 'wt');
+for ($inc = 0; $inc < $increment; $inc++) {
+	fwrite($fp, 'file ' . escapeshellarg($filename . '-' . $inc . '.' . $extension) . "\n"); 
+}
+fclose($fp);
+exec( $ffmpeg . ' -v quiet -f concat -i ' . escapeshellarg($filename . '.txt') . ' -c copy ' . $output );
+echo "\n";
+
+/* Clean up files */
+unlink($filename . '.txt');
+for ($inc = 0; $inc < $increment; $inc++) {
+	unlink($filename . '-' . $inc . '.' . $extension);
+}
